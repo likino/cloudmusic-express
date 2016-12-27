ba00fec6bcf1b0f4cd86eb708e930258333d42c4
Hitlee <lyr@liyiren.me>
2016-12-27 14:37:25 +0800

    跨域

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ app.use(express.static('./web'));
 var search = require('./routes/search');
 var song = require('./routes/song');
 
-// 跨域
-// app.all('*', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//     res.header("X-Powered-By",' 3.2.1')
-//     res.header("Content-Type", "application/json;charset=utf-8");
-//     next();
-// });
+// 跨域设置 本地测试使用
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 
 app.use(logger('dev'));
 
